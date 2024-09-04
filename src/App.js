fa58a50b2e0349ad15cf03d8a5360ba764c77dc4
Swastik Shetty <127413152+SwastikShetty06@users.noreply.github.com>
2024-09-04 15:46:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -34,12 +34,8 @@ function App() {
         <div>
           <PetCard pet={currentPet} />
           <div className="buttons">
-            <button onClick={handleSwipeLeft} className="swipe-btn">
-              Swipe Left
-            </button>
-            <button onClick={handleSwipeRight} className="swipe-btn">
-              Swipe Right
-            </button>
+          <button onClick={handleSwipeLeft} type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Swipe Left</button> 
+            <button onClick={handleSwipeRight} type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Swipe Right</button>
           </div>
         </div>
       ):(
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app.js",
+    "./src/components/Adopted.js",
+    "./src/components/PetCard.js",
+    "./src/data.js",
+    "./src/index.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
